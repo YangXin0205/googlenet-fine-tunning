efac9c713ee32273f6fb1e23a02be3b7ab9e599d
YangXin0205 <38850750+YangXin0205@users.noreply.github.com>
2018-04-30 15:34:41 +0800

    Create retrain.bat

---
+++ retrain.bat
@@ -0,0 +1,8 @@
+python E:/tensorflow/tensorflow-master/tensorflow-master/tensorflow/examples/image_retraining/retrain.py ^
+--bottleneck_dir bottleneck ^
+--how_many_training_steps 2000 ^
+--model_dir E:/tensorflow/tf_lesson/googleNet/inception_model ^
+--output_graph output_graph.pb ^
+--output_labels output_labels.txt ^
+--image_dir E:/tensorflow/tf_lesson/retrain/train_images ^
+pause
