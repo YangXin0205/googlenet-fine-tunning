ca5e7c2f38e50b8ae3fde47d2563b891c098d5a1
YangXin0205 <38850750+YangXin0205@users.noreply.github.com>
2018-05-21 10:43:59 +0800

    Update retrain.bat

--- retrain.bat
+++ retrain.bat
@@ -1,8 +1,10 @@
-python E:/tensorflow/tensorflow-master/tensorflow-master/tensorflow/examples/image_retraining/retrain.py ^
---bottleneck_dir bottleneck ^
---how_many_training_steps 2000 ^
---model_dir E:/tensorflow/tf_lesson/googleNet/inception_model ^
---output_graph output_graph.pb ^
---output_labels output_labels.txt ^
---image_dir E:/tensorflow/tf_lesson/retrain/train_images ^
+python D:/PycharmProjects/Test/retrain/googlenet_fine/retrain.py ^
+--bottleneck_dir D:/PycharmProjects/retrain/bottleneck ^
+--how_many_training_steps 300000 ^
+--model_dir D:/PycharmProjects/Test/googlenet ^
+--output_graph D:/PycharmProjects/Test/retrain/googlenet_fine/output_graph.pb ^
+--output_labels D:/PycharmProjects/Test/retrain/googlenet_fine/output_labels.txt ^
+--learning_rate 0.005 ^
+--train_batch_size 96 ^
+--image_dir D:\PycharmProjects\retrain\croped
 pause
