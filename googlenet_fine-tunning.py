d7afb99ba148e7fd56d02987a19ff5c7f6833385
YangXin0205 <38850750+YangXin0205@users.noreply.github.com>
2018-05-21 10:41:24 +0800

    Update googlenet_fine-tunning.py

--- googlenet_fine-tunning.py
+++ googlenet_fine-tunning.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import os
 import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-
+import shutil
+import align_dataset_mtcnn
+import sys
 
 lines = tf.gfile.GFile('output_labels.txt').readlines()
 uid_to_human = {}
@@ -12,6 +12,7 @@ for uid, line in enumerate(lines):
     # 去掉换行符
     line = line.strip('\n')
     uid_to_human[uid] = line
+
 
 
 def id_to_string(node_id):
@@ -26,43 +27,44 @@ with tf.gfile.FastGFile('output_graph.pb', 'rb') as f:
     graph_def.ParseFromString(f.read())
     tf.import_graph_def(graph_def, name='')
 
+
+
+align_dataset_mtcnn.main(align_dataset_mtcnn.parse_arguments(sys.argv[1:]))
+
 with tf.Session() as sess:
+
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-    # 遍历目录
-    for root, dirs, files in os.walk('test_images'):
-        for file in files:
-            # 载入图片
-            image_data = tf.gfile.FastGFile(os.path.join(root, file), 'rb').read()
+
+    path11 =r"D:\PycharmProjects\Test\retrain\test_crop\1"
+    num = 0
+    length = len(os.listdir(path11))
+
+    for file in os.listdir(path11):
+
+        if  os.path.isfile(os.path.join(path11,file)):
+
+            image_data = tf.gfile.FastGFile(os.path.join(path11, file), 'rb').read()
             predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})  # 图片格式是jpg格式
             predictions = np.squeeze(predictions)  # 把结果转为1维数据
 
-            # 打印图片路径及名称
-            image_path = os.path.join(root, file)
-            # print(image_path)
-            # 显示图片
-            # img = Image.open(image_path)
-            # plt.imshow(img)
-            # plt.axis('off')
-            # plt.show()
-
             # 排序
             top_k = predictions.argsort()[::-1]
-            # print(top_k)
-            # print(id_to_string(top_k[0]))
-            path = "E:\\tensorflow\\tf_lesson\\retrain\\test_images\\"
-            absPath = os.path.join(path,id_to_string(top_k[0]))
-            #对path路径下的图片进行分类
+
+            name = id_to_string(top_k[0])
+
+            if name in file:
+                num += 1
+            else:
+                print(file)
+
+            path = r"D:\PycharmProjects\Test\retrain\test_crop\1"
+            absPath = os.path.join(path, id_to_string(top_k[0]))
+            # 对path路径下的图片进行分类
             if not os.path.exists(absPath):
                 os.mkdir(absPath)
-                #os.renames重命名文件，从而实现移动文件
-                os.renames(os.path.join(root, file),os.path.join(absPath,file))
+                # os.renames重命名文件，从而实现移动文件
+                shutil.move(os.path.join(path, file), os.path.join(absPath, file))
             else:
-                os.renames(os.path.join(root, file), os.path.join(absPath, file))
-            #打印分类结果
-            # for node_id in top_k:
-            #     # 获取分类名称
-            #     human_string = id_to_string(node_id)
-            #     # 获取该分类的置信度
-            #     score = predictions[node_id]
-            #     print('%s (score = %.5f)' % (human_string, score))
+                shutil.move(os.path.join(path, file), os.path.join(absPath, file))
 
+    print("test acc: %d/%d = %.2f%%" %(num,length,num/length*100))
